14c2ba201236b038246151617b2d142dbc023fec
DmitryGaydabura <gaydabura.d@icloud.com>
2022-05-24 01:09:33 +0300

    0.1 Semi-playable board is now active!

--- src/Logic.java
+++ src/Logic.java
@@ -5,26 +5,26 @@ public class Logic {
 
 
     public static void startGame() throws UnsupportedEncodingException {
-        Figure WPawnA = new Figure("*P", true, "\u265F", "A2");
-        Figure WRookA = new Figure("*R", true, "\u265C", "A1");
-        Figure WKnightB = new Figure("*N", true, "\u265E", "B1");
-        Figure WKnightG = new Figure("*N", true, "\u265E", "G1");
-        Figure WBishopC = new Figure("*B", true, "\u265D", "C1");
-        Figure WBishopF = new Figure("*B", true, "\u265D", "F1");
-        Figure WQueen = new Figure("*Q", true, "\u265B", "D1");
-        Figure WKing = new Figure("*K", true, "\u265A", "E1");
+        Figure WPawnA = new Figure("*P", true, "♟", "A2");
+        Figure WRookA = new Figure("*R", true, "♜", "A1");
+        Figure WKnightB = new Figure("*N", true, "♞", "B1");
+        Figure WKnightG = new Figure("*N", true, "♞", "G1");
+        Figure WBishopC = new Figure("*B", true, "♝", "C1");
+        Figure WBishopF = new Figure("*B", true, "♝", "F1");
+        Figure WQueen = new Figure("*Q", true, "♛", "D1");
+        Figure WKing = new Figure("*K", true, "♚", "E1");
 
-        Figure BPawnA = new Figure(" P", true, "\u2659", "A7");
-        Figure BRookA = new Figure(" R", true, "\u2656", "A8");
-        Figure BRookH = new Figure(" R", true, "\u2656", "H8");
-        Figure BKnightB = new Figure(" N", true, "\u2658", "B8");
-        Figure BKnightG = new Figure(" N", true, "\u2658", "G8");
-        Figure BBishopC = new Figure(" B", true, "\u2657", "C8");
-        Figure BBishopF = new Figure(" B", true, "\u2657", "F8");
-        Figure BQueen = new Figure(" Q", true, "\u2655", "D8");
-        Figure BKing = new Figure(" K", true, "\u2654", "E8");
+        Figure BPawnA = new Figure(" P", true, "♙", "A7");
+        Figure BRookA = new Figure(" R", true, "♖", "A8");
+        Figure BRookH = new Figure(" R", true, "♖", "H8");
+        Figure BKnightB = new Figure(" N", true, "♘", "B8");
+        Figure BKnightG = new Figure(" N", true, "♘", "G8");
+        Figure BBishopC = new Figure(" B", true, "♗", "C8");
+        Figure BBishopF = new Figure(" B", true, "♗", "F8");
+        Figure BQueen = new Figure(" Q", true, "♕", "D8");
+        Figure BKing = new Figure(" K", true, "♔", "E8");
 
-        Figure empty = new Figure("  ", true, "\u2001", "A3");
+        Figure empty = new Figure("  ", true, " ", "A3");
 
 
         Figure[] allFigures = new Figure[64];
